3fdbd058588d4b1faec7362de3a918c8878e63a5
amank <aman.kaushik.975@gmail.com>
2023-08-21 12:56:12 +0530

    Try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,38 @@
 package com.driver;
 
 public class Main {
+    // Task 1
+    static class Product {
+        // Task 3
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        // Task 4
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Task 2
+        Product p = new Product();
+
+
+        int result1 = p.product(3, 4);
+        System.out.println("Result 1: " + result1);
+
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Result 2: " + result2);
+
+
+        double result3 = p.product(2.5, 1.5);
+        System.out.println("Result 3: " + result3);
+    }
 }
